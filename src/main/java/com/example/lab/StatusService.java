287ea3b1b664206c564fd6ca165ddf5832549739
Hendrik Schuster <hendrik.schuster@consileon.de>
2023-12-12 13:07:10 +0100

    created repo

---
+++ src/main/java/com/example/lab/StatusService.java
@@ -0,0 +1,18 @@
+package com.example.lab;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatusService {
+    public String processStatus(boolean arriving) {
+        if (arriving) {
+            return "springfundamentals/hello";
+        } else {
+            return "springfundamentals/goodbye";
+        }
+    }
+
+    public String congratulate() {
+        return "springfundamentals/success";
+    }
+}
